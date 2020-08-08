c119b412031b233188b926e9b91e581edc28ae08
Mat Grube <m.grube@outlook.com>
2020-08-08 14:39:14 +0200

    Added API communications | Made code ASYNC

--- script/game.js
+++ script/game.js
@@ -6,12 +6,59 @@ const gameLogic = (activePlayer) => {
     return isWon;
 }
 
+async function getDeck() {
+    let res;
+    let deck = fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    .then(response => response.json())
+    .then(data => {return data});
+    
+    res = await deck;
+    return res;
+}
+
+async function drawCard(id) {
+    let res;
+    let card = fetch(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=1`)
+    .then(response => response.json())
+    .then(data => {return data});
+    res = await card;
+    return res;
+}
+
+function didPlayerLose(playersScore, currentPlayer) {
+    playersScore[0] += playersScore[currentPlayer][3].forEach(element => Number(element.value));
+    if(playersScore[currentPlayer][0] > 21) playersScore[currentPlayer][2] = true;
+    console.log(playersScore[currentPlayer][0]);
+}
+
+async function playAsPlayer(deckId, playersScore, currentPlayer) {
+    let newCard = await drawCard(deckId);
+    playersScore[currentPlayer][1] += 1;
+    console.log(typeof(playersScore[currentPlayer][3]));
+    await playersScore[currentPlayer][3].push(newCard.cards[0]);
+    console.log(playersScore);
+    if (playersScore[currentPlayer][1] == 1) playAsPlayer(deckId, playersScore, currentPlayer);
+    didPlayerLose(playersScore, currentPlayer);
+}
+
+async function playGame(playersScore) {
+    let currentPlayer = 0;
+    let deck = await getDeck();
+    console.log(deck.deck_id);
+
+    // playersScore.forEach(element => {
+    //     element[0] = 0;
+    // });
+
+    playAsPlayer(deck.deck_id, playersScore, currentPlayer);
+}
+
 
 const gamePlay = (playersScore) => {
     const addPlayer = document.getElementById('addPlayer');
     
     const playerAdd = () => {
-        playersScore.length < 4 ? playersScore.push([0, 0]) : console.log("Max players already!");
+        playersScore.length < 4 ? playersScore.push([0, 0, false, []]) : console.log("Max players already!");
         };
     addPlayer.onclick = playerAdd;
 
@@ -22,7 +69,8 @@ const gamePlay = (playersScore) => {
 
 (function table () {
 
-    const playersScore = [[0,0]];
+    let activePlayer = 0;
+    const playersScore = [[0, 0, false, []]];
     
     function moduleMaker(type, moduleClass, id) {
         const result = document.createElement(type);
@@ -43,6 +91,8 @@ const gamePlay = (playersScore) => {
     const startGame = moduleMaker('button', 'startGame', 'startGame');
     startGame.innerText = "Rozpocznij grę";
 
+    const playersCards = moduleMaker('div', `player_${activePlayer}_hand`);
+
     game.appendChild(gameElement);
     gameElement.appendChild(tableElement);
     tableElement.appendChild(addPlayer);
@@ -54,6 +104,10 @@ const gamePlay = (playersScore) => {
         console.log(playersScore);
     }
 
+    playGame(playersScore);
+
+    
     gamePlay(playersScore);
+    console.log(playersScore);
 })();
 
